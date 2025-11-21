97544e68bf154b41b947bd72e1b23e144988c777
neon1024 <robertscumnpu@gmail.com>
2025-11-21 21:23:35 +0200

    backend init + postgresql db connection

---
+++ backend/src/main/java/com/neon1024/backend/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.neon1024.backend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")  // apply to all endpoints
+                        .allowedOrigins("http://localhost:3000")  // allowed origins
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
