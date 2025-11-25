a4aa094d70f02ff9591d523eba1ac0c8fcb28c15
neon1024 <robertscumnpu@gmail.com>
2025-11-25 20:41:08 +0200

    ProductCard

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -2,15 +2,44 @@ import { Typography } from "@mui/material";
 
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
 
 import Product from "../models/Product";
 
 interface ProductCardProps {
-    product: Product;
+    product: Product | null;
 }
 
 function ProductCard({ product }: ProductCardProps) {
-    return <Typography>Product</Typography>;
+    return (
+        <>
+            <Typography>Product</Typography>
+
+            <Card variant="outlined">
+                <CardContent>
+                    <CardMedia
+                        sx={{ height: 200 }}
+                        image={product?.getImageUrl()}
+                        title={product?.getName()}
+                    />
+                    <Typography>Nume: {product?.getName()}</Typography>
+                    <Typography>Categorie: {product?.getCategory()}</Typography>
+                    <Typography>
+                        Descriere: {product?.getDescription()}
+                    </Typography>
+                    <Typography>
+                        Pret:{" "}
+                        {product?.getPrice() + " " + product?.getCurrency()}
+                    </Typography>
+                    <Typography>Cantitate: {product?.getQuantity()}</Typography>
+                    <Typography>
+                        Greutate:{" "}
+                        {product?.getWeight() + "" + product?.getUnit()}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </>
+    );
 }
 
 export default ProductCard;
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,7 +1,5 @@
 import { Typography } from "@mui/material";
 import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
-import { styled } from "@mui/material/styles";
 import { NavLink } from "react-router-dom";
 
 import ProductCard from "../components/ProductCard";
@@ -10,26 +8,103 @@ import Product from "../models/Product";
 
 function Dashboard() {
     const products = [
-        new Product("1", "Pui", "Food", "Pui", 50, "RON", 1, 10, "kg", ""),
-        new Product("2", "Pui", "Food", "Pui", 50, "RON", 1, 10, "kg", ""),
-        new Product("3", "Pui", "Food", "Pui", 50, "RON", 1, 10, "kg", ""),
-        new Product("4", "Pui", "Food", "Pui", 50, "RON", 1, 10, "kg", ""),
-        new Product("5", "Pui", "Food", "Pui", 50, "RON", 1, 10, "kg", ""),
-        new Product("6", "Pui", "Food", "Pui", 50, "RON", 1, 10, "kg", ""),
-        new Product("7", "Pui", "Food", "Pui", 50, "RON", 1, 10, "kg", ""),
-        new Product("8", "Pui", "Food", "Pui", 50, "RON", 1, 10, "kg", ""),
+        new Product(
+            "1",
+            "Pui",
+            "Food",
+            "Pui",
+            50,
+            "RON",
+            1,
+            10,
+            "kg",
+            "../public/fat chicken.jpg"
+        ),
+        new Product(
+            "2",
+            "Pui",
+            "Food",
+            "Pui",
+            50,
+            "RON",
+            1,
+            10,
+            "kg",
+            "../public/fat chicken.jpg"
+        ),
+        new Product(
+            "3",
+            "Pui",
+            "Food",
+            "Pui",
+            50,
+            "RON",
+            1,
+            10,
+            "kg",
+            "../public/fat chicken.jpg"
+        ),
+        new Product(
+            "4",
+            "Pui",
+            "Food",
+            "Pui",
+            50,
+            "RON",
+            1,
+            10,
+            "kg",
+            "../public/fat chicken.jpg"
+        ),
+        new Product(
+            "5",
+            "Pui",
+            "Food",
+            "Pui",
+            50,
+            "RON",
+            1,
+            10,
+            "kg",
+            "../public/fat chicken.jpg"
+        ),
+        new Product(
+            "6",
+            "Pui",
+            "Food",
+            "Pui",
+            50,
+            "RON",
+            1,
+            10,
+            "kg",
+            "../public/fat chicken.jpg"
+        ),
+        new Product(
+            "7",
+            "Pui",
+            "Food",
+            "Pui",
+            50,
+            "RON",
+            1,
+            10,
+            "kg",
+            "../public/fat chicken.jpg"
+        ),
+        new Product(
+            "8",
+            "Pui",
+            "Food",
+            "Pui",
+            50,
+            "RON",
+            1,
+            10,
+            "kg",
+            "../public/fat chicken.jpg"
+        ),
     ];
-
-    const Item = styled(Paper)(({ theme }) => ({
-        backgroundColor: "#fff",
-        ...theme.typography.body2,
-        padding: theme.spacing(1),
-        textAlign: "center",
-        color: (theme.vars ?? theme).palette.text.secondary,
-        ...theme.applyStyles("dark", {
-            backgroundColor: "#1A2027",
-        }),
-    }));
 
     return (
         <>
@@ -38,10 +113,6 @@ function Dashboard() {
             <Grid container spacing={2}>
                 {products.map((product) => (
                     <Grid key={product.getId()} size={2}>
-                        <Item>
-                            <Typography>{product.getName()}</Typography>
-                            <Typography>{product.getQuantity()}</Typography>
-                        </Item>
                         <ProductCard product={product} />
                     </Grid>
                 ))}
