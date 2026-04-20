456e99b071d841a3921b9c62134246770c595444
neon1024 <robertscumnpu@gmail.com>
2026-04-20 15:52:16 +0300

    Update ImagesController.java

--- backend/src/main/java/com/neon1024/backend/controllers/ImagesController.java
+++ backend/src/main/java/com/neon1024/backend/controllers/ImagesController.java
@@ -1,30 +1,24 @@
 package com.neon1024.backend.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.neon1024.backend.common.ApiResponse;
+import com.neon1024.backend.models.ImageDTO;
 import com.neon1024.backend.repositories.ProductsRepository;
 import com.neon1024.backend.services.ImagesService;
-
-import java.util.List;
-
-import java.util.UUID;
-
-import com.neon1024.backend.models.ImageDTO;
-
-import java.util.Map;
-
-import org.springframework.http.MediaType;
-
-import com.neon1024.backend.common.ApiResponse;
 
 @RestController
 @RequestMapping("/images")
